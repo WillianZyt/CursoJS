05eb35e78759439d7834a108e724f55583628926
Willian Zytkoski <88804245+WillianZyt@users.noreply.github.com>
2024-02-07 13:48:17 -0300

    s

--- JS/aula 7/index.js
+++ JS/aula 7/index.js
@@ -10,7 +10,10 @@
 const nome = 'Joao';
 console.log(nome);
 
-const primeiroNumero = 5;
+const primeiroNumero = 5; //const nao pode ser alterado, mais seguro
 const segundoNumero = 10;
 const resultado = primeiroNumero * segundoNumero;
-console.log(resultado);
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;  //let pode ser alterado, perde o primeiro numero
+console.log(resultadoTriplicado);
