03b5110ff9881d3121bc04737c05c6be9cf8041f
Willian Zytkoski <88804245+WillianZyt@users.noreply.github.com>
2024-02-07 13:07:14 -0300

    s

--- JS/aula 6/index.js
+++ JS/aula 6/index.js
@@ -1,7 +1,21 @@
+//Nao pode usar palavras reservadas, como "let,if"
+//Variaveis precisam de nomes significativos
+//Nao pode redeclarar com let, apenas sem o let
+//NAO UTILIZE VAR, UTILIZE LET
 
+let nome = 'Joao';  //String
+nome = 'Felipe'
+console.log(nome,' nasceu em 1984.');
+console.log('Em 2000 ', nome,' conheceu Maria.');
+console.log(nome,' casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com ', nome,' em 2015.');
+console.log('O filho de ', nome,' se chama Eduardo.');
 
-console.log('Joao nasceu em 1984.')
-console.log('Em 2000 Joao conheceu Maria.')
-console.log('Joao casou-se com Maria em 2012.')
-console.log('Maria teve 1 filho com Joao em 2015.')
-console.log('O filho de Joao se chama Eduardo.')
+//Nao pode comecar o nome de uma variavel com numero, como "1nome"
+//Nao podem conter espacos ou tracos
+//Usado camalCase
+//Case-sensitive, diferencia maiuscula de minuscula
+
+let nomeCompletoDoCliente = 'Willian';
+console.log(nomeCompletoDoCliente);
+
---
+++ JS/aula 7/index.js
@@ -0,0 +1,16 @@
+//Nao podemos criar constanter com palavras reservadas, como "let,if"
+//Constanter precisam ter nomes significativos
+//Nao pode comecar o nome de uma constanter com numero, como "1nome"
+//Nao podem conter espacos ou tracos
+//Utilizamos camalCase
+//Case-sensitive
+//Nao pode redeclarar uma constante
+//NAO UTILIZE VAR, UTILIZE CONST
+
+const nome = 'Joao';
+console.log(nome);
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+console.log(resultado);
