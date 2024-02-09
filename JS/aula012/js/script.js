94d4dac6e563d0a7b00fa2c9684d5652c53afd73
Willian Zytkoski <88804245+WillianZyt@users.noreply.github.com>
2024-02-09 07:08:50 -0300

    s

--- JS/aula012/js/script.js
+++ JS/aula012/js/script.js
@@ -2,3 +2,9 @@ let num1 = Number(prompt('Qual o primeiro número?'));
 let num2 = Number(prompt('Qual o segundo número?'));
 let resultado = num1 + num2;
 alert(`Seu número é ${resultado}`);
+
+let varA = 'A'; // B
+let varB = 'B'; // C
+let varC = 'C'; // A 
+[varA, varB, varC] = [varB, varC, varA]
+console.log(varA, varB, varC);
