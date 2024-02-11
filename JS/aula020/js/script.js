11c3679f5918c10e2f8b17011f21573eb9074bb4
Willian Zytkoski <88804245+WillianZyt@users.noreply.github.com>
2024-02-11 12:17:49 -0300

    s

--- JS/aula020/js/script.js
+++ JS/aula020/js/script.js
@@ -11,10 +11,18 @@ function meuEscopo  ()   {
         const sobrenome = form.querySelector('.sobrenome');
         const peso = form.querySelector('.peso');
         const altura = form.querySelector('.altura');
-        pessoas.push([nome,sobrenome, peso, altura]);
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+        console.log(pessoas);
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ` + `tem ${peso.value}kgs e mede ${altura.value} de altura.</p>`;
 
     }
     form.addEventListener('submit', recebeEventoForm);
-    console.log(pessoas[0]);
+
+
 }
 meuEscopo();
