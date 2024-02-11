209e0684352b22e7999949b6a2d13d9bb1fc1321
Willian Zytkoski <88804245+WillianZyt@users.noreply.github.com>
2024-02-11 17:19:21 -0300

    s

--- JS/aula026/assets/js/main.js
+++ JS/aula026/assets/js/main.js
@@ -1,1 +1,15 @@
-alert(1);
+function meuEscopo() {
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+
+        function recebeEventoForm (evento)   {
+            evento.preventDefault();
+
+            const peso = form.querySelector('.peso');
+            const altura = form.querySelector('.altura');
+            const imc = peso * (altura * altura);
+            console.log(peso, altura);
+            resultado.innerHTML = `<p>Seu IMC é de ${imc}.</p>`
+        }
+     }
+meuEscopo();
