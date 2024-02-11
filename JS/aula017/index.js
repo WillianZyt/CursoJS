ea795ac62d2fa9d1fe71773cec0ca16fe14a082f
Willian Zytkoski <88804245+WillianZyt@users.noreply.github.com>
2024-02-11 07:50:07 -0300

    s

--- JS/aula017/index.js
+++ JS/aula017/index.js
@@ -1,4 +1,22 @@
-function saudacao(){
+// function saudacao(nome){
+//     console.log(`Bom dia ${nome}`);
+// }
+// saudacao()
+// const variavel = saudacao('Willian');
+// saudacao('Felipe');
 
-}
-saudacao();
+// function soma(x = 1, y = 1){
+//     const resultado = x + y;
+//     return resultado;
+// }
+
+// console.log(soma('Ola', ' mundo.'));        // String ou valor
+// console.log(soma(4));
+
+
+// const raiz = function(n) {
+//     return n ** 0.5;
+// }
+const raiz = n => n ** 0.5;     // Function simplificada
+
+console.log(raiz(9));
