5e9469e07e0d67ab86f2c435c101e7c909447bb9
Willian <willianzyt@outlook.com>
2024-02-11 21:15:18 -0300

    s

--- JS/aula026/assets/js/main.js
+++ JS/aula026/assets/js/main.js
@@ -1,16 +1,63 @@
 // Capturar evento de submit do formulário
 const form = document.querySelector('#formulario');
 
-form.addEventListener('submit', function(e){        //e = evento
+form.addEventListener('submit', function(e){        // e = evento
     e.preventDefault();
-    console.log('Evento prevenido.');
-    setResultado('Olá mundo!');
+    const inputPeso = e.target.querySelector('#peso');   // e. -> pois na função ja selecionou o submit
+    const inputAltura = e.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+    
+    if (!peso) {
+        setResultado('Peso inválido.', false);
+        return; 
+    }
+    if (!altura) {
+        setResultado('Altura inválida.', false);
+        return; 
+    }
+
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+
+    setResultado(msg, true);
 });
 
-function setResultado (msg) {
+function getNivelImc (imc){
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade Gral 1', 'Obesidade Gral 2', 'Obesidade Gral 3'];
+    
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
+}
+
+function getImc(peso, altura){
+    imc = peso / altura ** 2;
+    return imc.toFixed(2);
+}
+
+function criaP() {
+    const p = document.createElement('p');  //Criar elementro novo-> p= paragrafo
+    p.classList.add('paragrafo-resultado');
+    return p;
+}
+
+function setResultado (msg, isValid) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
-    const p = document.createElement('p');  //Criar elementro novo-> p= paragrafo
-    p.innerHTML = 'Qualquer coisa.';
+
+    const p = criaP();
+    if (isValid){
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
+    p.innerHTML = msg;
     resultado.appendChild(p);
 }
