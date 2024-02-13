b0392be867b5c35446b3efb9148b5dae78f5aefe
Willian Zytkoski <88804245+WillianZyt@users.noreply.github.com>
2024-02-13 08:24:13 -0300

    s

--- JS/aula030/assets/js/main.js
+++ JS/aula030/assets/js/main.js
@@ -1,4 +1,66 @@
 
-const data = new Data();
+const data = new Date('2019-10-07 22:52');
+let diaSemana = data.getDay();
+let dia = data.getDate();
+let mes = data.getMonth();
+let ano = data.getFullYear();
+let hora = data.getHours();
+let min = data.getMinutes();
 
-console.log(data);
+function getDiaSemanaTexto(diaSemana) {
+    let diaSemanaTexto;
+    if (diaSemana === 0) {
+        return diaSemanaTexto= 'Domingo';
+    } else if (diaSemana === 1) {
+        return diaSemanaTexto= 'Segunda-feira';
+    } else if (diaSemana === 2) {
+        return diaSemanaTexto= 'Terça-feira';
+    } else if (diaSemana === 3) {
+        return diaSemanaTexto= 'Quarta-feira';
+    } else if (diaSemana === 4) {
+        return diaSemanaTexto= 'Quinta-feira';
+    } else if (diaSemana === 5) {
+        return diaSemanaTexto= 'Sexta-feira';
+    } else if (diaSemana === 6) {
+        return diaSemanaTexto= 'Sábado';
+    }
+}
+
+function getMesTexto(mes) {
+    let mesTexto;
+    if (mes === 0) {
+        return mesTexto= 'Janeiro';
+    } else if (mes === 1) {
+        return diaSemanaTexto= 'Fevereiro';
+    } else if (mes === 2) {
+        return diaSemanaTexto= 'Março';
+    } else if (mes === 3) {
+        return diaSemanaTexto= 'Abril';
+    } else if (mes === 4) {
+        return diaSemanaTexto= 'Maio';
+    } else if (mes === 5) {
+        return diaSemanaTexto= 'Junho';
+    } else if (mes === 6) {
+        return diaSemanaTexto= 'Julho';
+    } else if (mes === 7) {
+        return diaSemanaTexto= 'Agosto';
+    } else if (mes === 8) {
+        return diaSemanaTexto= 'Setembro';
+    } else if (mes === 9) {
+        return diaSemanaTexto= 'Outubro';
+    } else if (mes === 10) {
+        return diaSemanaTexto= 'Novembro';
+    } else if (mes === 11) {
+        return diaSemanaTexto= 'Dezembro';
+    }
+}
+
+
+let = mesTexto = getMesTexto(mes);
+let = diaSemanaTexto = getDiaSemanaTexto(diaSemana);
+let diaDeHoje =document.querySelector('.diaDeHoje');
+diaDeHoje.innerHTML = `${diaSemanaTexto}, ${dia} de ${mesTexto} de ${ano} </br> ${hora}:${min}`;
+
+
+
+console.log(`${diaSemanaTexto}, ${dia} de ${mesTexto} de ${ano} \n ${hora}:${min}`);
