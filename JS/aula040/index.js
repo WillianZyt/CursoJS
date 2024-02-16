78f479a503dbcab1e49a1d7b1243b4b20d42ec4c
Willian <willianzyt@outlook.com>
2024-02-16 17:11:42 -0300

    s

--- JS/aula040/index.js
+++ JS/aula040/index.js
@@ -1,9 +1,17 @@
+
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 for (let numero of numeros){
 
-    if(numero === 2){
+    if(numero === 2){   // Pula a interação, e segue o próximo laço
+        console.log('Pulei o número 2');
         continue;
+    }
+    if(numero === 5){
+        continue;
+    }
+    if(numero === 7){
+        break;
     }
 console.log(numero);
 }
