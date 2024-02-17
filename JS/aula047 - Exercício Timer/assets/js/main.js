680235cc60736fae8668029e2d2b4cefa21f74bc
Willian <willianzyt@outlook.com>
2024-02-17 20:55:12 -0300

    s

--- JS/aula047 - Exercício Timer/assets/js/main.js
+++ JS/aula047 - Exercício Timer/assets/js/main.js
@@ -1,24 +1,14 @@
+// new Date(0)
 const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
 
-
-
-const timer = setInterval(function timerUS(){
-    for (let i; i <=59; i++){
-        console.log = `00:00:${i}`
-    }
-}, 1000)
-timer();
-
-const relogioTimer = new timer();
-
 relogio.addEventListener('click', function(event){
 
 });
 iniciar.addEventListener('click', function(event){
-    timerUS();
+
 });
 pausar.addEventListener('click', function(event){
 
