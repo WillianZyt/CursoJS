1d5948e8645ae2e0e33b16c895d54bb6fdbdb72e
Willian Zytkoski <88804245+WillianZyt@users.noreply.github.com>
2024-02-17 11:47:27 -0300

    s

--- JS/aula043/index.js
+++ JS/aula043/index.js
@@ -6,11 +6,33 @@
 // Checar se o número é realmente um número = Retorna o próprio número
 // Use a função com o númesros de 0 a 100 (um laço)
 
-
-function numDiv5e3(){
-    for (let i= 0; i<= 10; i++){
-        if (i % 3 === 0) return 'Fizz'; else if(i % 5 === 0) return 'Buzz';
-        console.log(i);
-    }     
+function fizzBuzz(numero) {
+    if (typeof numero !== 'number') return numero;
+    if (numero % 3 ===0 && numero % 5 === 0) return 'FizzBuzz';
+    if (numero % 3 === 0) return 'Fizz';
+    if (numero % 5 === 0) return 'Buzz';
+    return numero;
 }
-numDiv5e3();
+for (let i = 0; i <= 100; i++){
+    console.log(i, fizzBuzz(i));
+}
+
+// MINHA RESOLUÇÃO
+// function numDiv5e3(){
+//     for (let i= 1; i<= 100; i++){
+//         if (i % 3 ===0 && i % 5 === 0){
+//             console.log('FizzBuzz');
+//             continue;
+//         }
+//         if (i % 3 === 0){
+//             console.log('Fizz');
+//             continue;
+//         }
+//       if (i % 5 === 0){
+//             console.log('Buzz');
+//             continue;
+//         }
+//         console.log(i, Number(i));
+//     }
+// }
+// numDiv5e3();
