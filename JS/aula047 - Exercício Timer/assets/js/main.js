30f9aad8532f81ca30ee3b92ff8c6ea465d3af69
Willian Zytkoski <88804245+WillianZyt@users.noreply.github.com>
2024-02-19 13:06:11 -0300

    s

--- JS/aula047 - Exercício Timer/assets/js/main.js
+++ JS/aula047 - Exercício Timer/assets/js/main.js
@@ -1,10 +1,26 @@
 // new Date(0)
 const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
 let segundos = 0;
 let timer;
+
+document.addEventListener('click', function (e) {
+    el = e.target;
+    if (el.classList.contains('iniciar')) {
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        iniciaRelogio();
+    }
+    if (el.classList.contains('pausar')) {
+        relogio.classList.add('pausado')
+        clearInterval(timer);
+    }
+    if (el.classList.contains('zerar')) {
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        relogio.innerHTML = '00:00:00';
+        segundos = 0;
+    }
+})
 
 function criaHoraDosSegundos(segundos) {
     const data = new Date(segundos * 1000);
@@ -20,18 +36,3 @@ function iniciaRelogio() {
         relogio.innerHTML = criaHoraDosSegundos(segundos);
     }, 1000);
 }
-
-iniciar.addEventListener('click', function (event) {
-    relogio.classList.remove('pausado');
-    clearInterval(timer);
-    iniciaRelogio();
-});
-pausar.addEventListener('click', function (event) {
-    relogio.classList.add('pausado')
-    clearInterval(timer);
-});
-zerar.addEventListener('click', function (event) {
-  clearInterval(timer);
-  relogio.innerHTML = '00:00:00';
-  segundos = 0;
-});
