18537e1fdcf46fe8eaa345dff878767a2843bbb7
Willian Zytkoski <88804245+WillianZyt@users.noreply.github.com>
2024-02-23 07:08:43 -0300

    s

--- JS/aula053/index.js
+++ JS/aula053/index.js
@@ -1,1 +1,11 @@
-// Closures
+// Closures, habilidade da funcao de acessar o escopo lexico, global, etc...
+function retornaFuncao(nome) {
+    return function(){
+        return nome;
+    };
+}
+const funcao = retornaFuncao('Luiz');
+const funcao2 = retornaFuncao('João');
+console.dir(funcao);
+console.dir(funcao2);
+console.log(funcao(), 'e', funcao2());
