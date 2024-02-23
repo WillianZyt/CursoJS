1990504747a28221ec869715822ddce03f2d2e50
Willian Zytkoski <88804245+WillianZyt@users.noreply.github.com>
2024-02-23 15:33:01 -0300

    s

--- JS/aula056/index.js
+++ JS/aula056/index.js
@@ -1,13 +1,37 @@
-// Factory functions
-function criaPessoa(nome, sobrenome){
+// Factory functions (Função fábrica)
+function criaPessoa(nome, sobrenome, altura, peso) {
     return {
       nome,
       sobrenome,
-      fala(assunto) {   // mesmo que -> fala: function(assunto) {}
+      // Getter
+      get nomeCompleto() {
+        return `${nome} ${sobrenome}`;
+      },
+      // Setter
+      set nomeCompleto(valor) {
+        valor = valor.split(" "); // split -> para dividir, e ('') é onde esta separando. E vai retornar um array
+        console.log(valor);
+      },
+  
+      fala(assunto) {
+        // mesmo que -> fala: function(assunto) {}
         return `${this.nome} está ${assunto}.`;
       },
-      // peso: 80
+      altura: altura,
+      peso: peso,
+      // Getter
+      get imc() {
+        // get-> fingi ser um atributo do objeto
+        const indice = this.peso / this.altura ** 2;
+        return indice.toFixed(2);
+      }
     };
   }
-  const p1 = criaPessoa('Willian', 'Zytkoski');
-  console.log(p1.fala('falando sobre JS.'));
+  const p1 = criaPessoa("Willian", "Zytkoski", 1.85, 92);
+  const p2 = criaPessoa("Ana Crisitna", "M. Zytkoski", 1.6, 56);
+  console.log(p2.fala("falando sobre JS."));
+  // console.log(p2.imc());
+  console.log(p2.imc);
+  console.log(p2.nomeCompleto);
+  p2.nomeCompleto = 'Maria Oliveira Silva'
+  
