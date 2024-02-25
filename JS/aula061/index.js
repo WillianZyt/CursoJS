2b760c57846d64d3f6b07d2e9e761fb33bbc2d8b
Willian Zytkoski <88804245+WillianZyt@users.noreply.github.com>
2024-02-25 16:51:11 -0300

    s

--- JS/aula061/index.js
+++ JS/aula061/index.js
@@ -8,38 +8,72 @@ function* geradora1() {
     yield "Valor 2";
     // Código qualquer...
     yield "Valor 3";
-  }
-  
-  // const g1 = geradora1();
-  // // console.log(g1);   .next -> para mostrar o sobre o objeto
-  // console.log(g1.next().value); // .next().value -> para mostrar o valor
-  //Valor 1
-  // console.log(g1.next().value);
-  //Valor 2
-  // console.log(g1.next());
-  // [object Object]
-  // {
-  //   "value": "Valor 3",
-  //   "done": false
-  // }
-  // console.log(g1.next());
-  // [object Object]
-  // {
-  //   "value": undefined,
-  //   "done": true
-  // }
-  
-  function* geradora2() {
+}
+
+// const g1 = geradora1();
+// // console.log(g1);   .next -> para mostrar o sobre o objeto
+// console.log(g1.next().value); // .next().value -> para mostrar o valor
+//Valor 1
+// console.log(g1.next().value);
+//Valor 2
+// console.log(g1.next());
+// [object Object]
+// {
+//   "value": "Valor 3",
+//   "done": false
+// }
+// console.log(g1.next());
+// [object Object]
+// {
+//   "value": undefined,
+//   "done": true
+// }
+
+function* geradora2() {
     let i = 0;
-  
+
     while (true) {
-      yield i;
-      i++;
+        yield i;
+        i++;
     }
-  }
-  // const g2 = geradora2();
-  // console.log(g2.next().value);
-  // console.log(g2.next().value);
-  // console.log(g2.next().value);
-  // console.log(g2.next().value
-  
+}
+// const g2 = geradora2();
+// console.log(g2.next().value);
+// console.log(g2.next().value);
+// console.log(g2.next().value);
+// console.log(g2.next().value
+
+
+function* geradora3() {
+    yield 0;
+    yield 1;
+    yield 2;
+}
+
+function geradora4() {
+    yield* geradora3();
+    yield 3;
+    yield 4;
+    yield 5;
+}
+
+const g4 = geradora4();
+for (let valor of g4) {
+    console.log(valor);
+}
+
+function* geradora5() {
+    yield function() {
+        console.log('Vim do y1')
+    };
+                                        // se colocar um return no meio, a funcao acaba nele
+    yield function() {
+        console.log('Vim do y2')
+    };
+}
+const g5 = geradora5();
+const func1 = g5.next().value;
+const func2 = g5.next().value;
+
+func1();
+func2();
