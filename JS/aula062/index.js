e9b02e7791fee918ce5f98cb644b6c5a807afb26
Willian <willianzyt@outlook.com>
2024-02-26 16:07:24 -0300

    s

--- JS/aula062/index.js
+++ JS/aula062/index.js
@@ -1,11 +1,18 @@
 // Valor por referência
 //
-// const nomes = ['Eduardo', 'Maria', 'Joana']; // Array literal
-const nomes = new Array('Eduardo', 'Maria', 'Joana');
-const novo = [...nomes];
+const nomes = ['Eduardo', 'Maria', 'Joana']; // Array literal
+// const nomes = new Array('Eduardo', 'Maria', 'Joana');
+// const novo = [...nomes];
 
-novo.pop();
+// novo.pop();  //.pop -> Remove o último elemento de um array
+// console.log(nomes);
+// //["Eduardo","Maria","Joana"]
+// console.log(novo);
+// //["Eduardo","Maria"]
+
+// const removido = nomes.shift(); // .shift -> Remove o primeiro elemento de um array 
+// nomes.push('João');  // .push -> Insere um elemento no inicio do array
+// nomes.push('Wallace');  
+nomes.unshift('João');  // .unshift -> Insere um elemento no inicio do array 
+nomes.unshift('Wallace');   
 console.log(nomes);
-//["Eduardo","Maria","Joana"]
-console.log(novo);
-//["Eduardo","Maria"]
