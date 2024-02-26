2678e0364c8af9858e4f1055f1a7bdf49b042dd3
Willian <willianzyt@outlook.com>
2024-02-26 19:20:03 -0300

    s

--- JS/aula062/index.js
+++ JS/aula062/index.js
@@ -1,6 +1,8 @@
 // Valor por referência
-//
-const nomes = ['Eduardo', 'Maria', 'Joana']; // Array literal
+//                 0          1       2
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana']; // Array literal
+// const novo = nomes.slice(0,-1); // .slice -> fatia, do elemento x ao y
+// console.log(novo);  // cortou 1 antes do ultimo escrito
 // const nomes = new Array('Eduardo', 'Maria', 'Joana');
 // const novo = [...nomes];
 
@@ -13,6 +15,14 @@ const nomes = ['Eduardo', 'Maria', 'Joana']; // Array literal
 // const removido = nomes.shift(); // .shift -> Remove o primeiro elemento de um array 
 // nomes.push('João');  // .push -> Insere um elemento no inicio do array
 // nomes.push('Wallace');  
-nomes.unshift('João');  // .unshift -> Insere um elemento no inicio do array 
-nomes.unshift('Wallace');   
-console.log(nomes);
+// nomes.unshift('João');  // .unshift -> Insere um elemento no inicio do array 
+// nomes.unshift('Wallace');   
+// console.log(nomes);
+
+// const nome = 'Luiz Otávio Miranda';
+// const nomes = nome.split(' ');  // .split -> converte em array
+// console.log(nomes);
+
+const nomes = [ 'Luiz', 'Otávio', 'Miranda' ];
+const nome = nomes.join(' ');   // .join -> converte em array
+console.log(nome);  
