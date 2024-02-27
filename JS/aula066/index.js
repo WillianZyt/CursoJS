88702c564f9a087f1dfae7b84de66b031864b795
Willian Zytkoski <88804245+WillianZyt@users.noreply.github.com>
2024-02-27 15:06:56 -0300

    s

--- JS/aula066/index.js
+++ JS/aula066/index.js
@@ -1,9 +1,10 @@
 // Map
 // Dobre os números
 //               0  1  2   3   4  5  ...
-// const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
-// const dobroNumeros = numeros.map(valor => valor *2);
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const dobroNumeros = numeros.map(valor => valor *2);
 // console.log(dobroNumeros);
+
 
 // Para cada elemento:
 // Retorne uma string com o nome da pessoa
@@ -16,7 +17,24 @@ const pessoas = [
     { nome: "Letícia", idade: 19 },
     { nome: "Rosane", idade: 32 },
     { nome: "Wallece", idade: 47 }
-  ];
-  
-  const showName = pessoas.map(obj => obj.nome);
-  console.log(showName);
+];
+
+const showName = pessoas.map(obj => obj.nome);
+// console.log(showName);
+
+const pessoasNoName = pessoas.map(valor => valor.idade);
+// delete obj.nome;
+// console.log(pessoasNoName);
+// const idades = pessoas.map(function(obj){
+//   return {idade: obj.idade};
+// })
+// console.log(idades);
+const idades = pessoas.map(obj => ({idade: obj.idade}));
+// console.log(idades);
+
+
+const comIds = pessoas.map(function(obj, indice){
+  obj.id = (indice + 1) * 1000;
+  return obj;
+})
+console.log(pessoas);
