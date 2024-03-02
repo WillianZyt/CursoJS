f2f3ad65721206c57d59357d40d8bc4539ff725d
Willian Zytkoski <88804245+WillianZyt@users.noreply.github.com>
2024-03-02 15:45:01 -0300

    s

--- JS/aula074/index.js
+++ JS/aula074/index.js
@@ -5,7 +5,9 @@ function Pessoa(nome, sobrenome) {
     // this.nomeCompleto = () => 'ORIGINAL: ' + this.nome + ' ' + this.sobrenome;
 }
 
-Pessoa.prototype.nomeCompleto = () => this.nome + ' ' + this.sobrenome;
+Pessoa.prototype.nomeCompleto = function() {  // primeiro busca a função no objeto, depois segue para o proto
+  return this.nome + ' ' + this.sobrenome;
+}
 
 // instância
 const pessoa1 = new Pessoa('Luiz', 'O.');   // <- Pessoa = Função construtora
@@ -13,6 +15,7 @@ const data = new Date();   // <- Date = Função construtora
 
 // Pessoa.prototype.estouAqui = 'Hahahaha';    // .prototype -> Prototipo, primeiro criada, a base, Proto
 // Pessoa.prototype é o mesmo que pessoa1.__proto__ (console do Google Chrome)
+// 1º pessoa -> 2º Pessoa.protype -> 3º Object.prototype 
 
 console.dir(pessoa1);
 console.dir(data);
