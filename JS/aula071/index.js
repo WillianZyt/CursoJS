4e7fea3ebe2d8285c3eaed58ebc9ea34beb6a797
Willian Zytkoski <88804245+WillianZyt@users.noreply.github.com>
2024-03-02 10:03:12 -0300

    S

--- JS/aula071/index.js
+++ JS/aula071/index.js
@@ -2,10 +2,16 @@
 function Produto(nome, preco, estoque) {
     this.nome = nome;
     this.preco = preco;
-    this.estoque = estoque;
-
-    Object.defineProperties(this, 'estoque',)
-}
-
-const p1 = new Produto('Camiseta', 20, 3);
-console.log(p1);
+  
+    Object.defineProperty(this, "estoque", {
+      enumerable: true, // mostra a chave
+      value: estoque, // valor
+      writable: false, // se pode ou na alterar o valor
+      configurable: false // configurável
+    });
+  }
+  
+  const p1 = new Produto("Camiseta", 20, 3);
+  p1.estoque = 10000;
+  console.log(p1);
+  
