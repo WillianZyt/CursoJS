3c2f3d4baaeb25bf1184f7fcf03f571d07aa9135
Willian Zytkoski <88804245+WillianZyt@users.noreply.github.com>
2024-03-02 13:46:57 -0300

    s

--- JS/aula073/index.js
+++ JS/aula073/index.js
@@ -1,6 +1,6 @@
-// Object.values
-// Object.entries
-// Object.assign(des, any)
+// Object.values    (retorna os valores)
+// Object.entries   (retorna as chaves e os valores)
+// Object.assign(des, any)  -> para copiar o objeto/ spread é mais intuitivo
 // Object.getOwnPropertyDescriptor(o, 'prop')
 // ... (spread)
 
@@ -9,3 +9,20 @@
 // Object.freeze   (congela o objeto)
 // Object.defineProperties (define várias propriedades)
 // Object.defineProperty   (define uma propriedade)
+
+const produto = {nome: 'Produto', preco: 1.8};
+console.log(Object.values(produto));
+
+// Object.defineProperty(produto, 'nome', {
+//     writable: false,
+// })
+// console.log(Object.getOwnPropertyDescriptor(produto, 'nome'));
+
+// const caneca = {        //...(objeto) -> para copiar
+//     ...produto,
+//     material: 'porcelana'
+// };
+// caneca.nome ='Outro nome';
+// caneca.preco=2.5;
+// console.log(produto);
+// console.log(caneca);
