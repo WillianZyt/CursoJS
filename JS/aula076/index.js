e649c59aedd7f9ae5aed8f3491e81569608d5197
Willian <willianzyt@outlook.com>
2024-03-03 18:15:19 -0300

    s

--- JS/aula076/index.js
+++ JS/aula076/index.js
@@ -10,8 +10,30 @@ Produto.prototype.aumento = function(quantia) {
 Produto.prototype.desconto = function(quantia) {
     this.preco -= quantia;
 };
-const p1 = new Produto(
-    this.nome = 'caneca',
-    this.preco = 5
-)
-console.log(p1);
+function Camiseta(nome, preco, cor) {
+    Produto.call(this, nome, preco);
+    this.cor = cor;
+}
+
+Camiseta.prototype = Object.create(Produto.prototype);
+Camiseta.prototype.constructor = Camiseta; 
+Camiseta.prototype.aumento = function(porcentual) {
+    this.preco = this.preco + (this.preco * (porcentual / 100));
+}
+
+function Caneca (nome, preco, material) {
+    Produto.call(this, nome, preco);
+    this.material = material;
+}
+Caneca.prototype = Object.create(Camiseta.prototype);
+Caneca.prototype.constructor = Caneca;
+Caneca.prototype.aumento = function(porcentual) {
+    this.preco = this.preco + (this.preco * (porcentual / 100));
+}
+const caneca = new Caneca('Caneca', 5, 'Porcelana');
+console.log(caneca);
+
+const produto = new Produto('Gen', 111)
+const camiseta = new Camiseta('Regata', 7.5, 'Preta');
+// console.log(produto);
+// console.log(camiseta);
