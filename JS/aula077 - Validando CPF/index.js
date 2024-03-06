c8b2c77524a8b30b6ff03c582f40a8f9464e9e1f
Willian <willianzyt@outlook.com>
2024-03-06 11:33:53 -0300

    s

--- JS/aula077 - Validando CPF/index.js
+++ JS/aula077 - Validando CPF/index.js
@@ -17,29 +17,26 @@ Se o número digito for maior que 9, consideramos 0.
 let cpf;
 function verificaCPF(cpf) {
     let cpfLimpo = cpf.replace(/\D+/g, ''); // (/\D+/g, '') -> para remover todo item que NaN
-    cpfArray = Array.from(cpfLimpo);
+    let cpfArray = Array.from(cpfLimpo);
     cpfArray = cpfArray.slice(0, -2);
     let primDigDoCPF;
-    valPrimDigDoCPF = (num) => {
-        num = this.cpfArray;
+    valPrimDigDoCPF = () => {
+        let primDigCPFArray = cpfArray;
         function multiplicaCPF() {
-            cpfArray[0] = cpfArray[0] * 10;
-            cpfArray[1] = cpfArray[1] * 9;
-            cpfArray[2] = cpfArray[2] * 8;
-            cpfArray[3] = cpfArray[3] * 7;
-            cpfArray[4] = cpfArray[4] * 6;
-            cpfArray[5] = cpfArray[5] * 5;
-            cpfArray[6] = cpfArray[6] * 4;
-            cpfArray[7] = cpfArray[7] * 3;
-            cpfArray[8] = cpfArray[8] * 2;
-            return cpfArray;
+            for (let i =0; i<primDigCPFArray.length; i++ ){
+                primDigCPFArray[i] = primDigCPFArray[i] * ((primDigCPFArray.length+1) - i);
+            }
+            return primDigCPFArray;
         }
-        cpfArray = multiplicaCPF(cpfArray).reduce((ac, val) => {
+        primDigCPFArray = multiplicaCPF(primDigCPFArray).reduce((ac, val) => {
             return ac + val;
         }, 0);
-        cpfArray = 11 - (cpfArray % 11);
-        return cpfArray;
+        primDigCPFArray = 11 - (primDigCPFArray % 11);
+        primDigDoCPF = primDigCPFArray;
+        return primDigDoCPF;
     }
-    console.log(valPrimDigDoCPF());
+    primDigDoCPF = valPrimDigDoCPF(cpfArray)
+    cpfArray.push(primDigDoCPF)
+    console.log(cpfArray);
 }
 verificaCPF('705.484.450-52');
