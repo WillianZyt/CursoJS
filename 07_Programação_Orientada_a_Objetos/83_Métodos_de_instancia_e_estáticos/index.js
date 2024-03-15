ebe02f9511ebc94e80c8dcbc08d275db3e3e0821
Willian <willianzyt@outlook.com>
2024-03-15 11:46:09 -0300

    s

--- 07_Programação_Orientada_a_Objetos/83_Métodos_de_instancia_e_estáticos/index.js
+++ 07_Programação_Orientada_a_Objetos/83_Métodos_de_instancia_e_estáticos/index.js
@@ -3,12 +3,22 @@ class ControleRemoto {
         this.tv = tv;
         this.volume = 0
     }
-
+    //  Método de instância
     aumentarVolume() {
         this.volume += 2
     }
     diminuirVolume() {
         this.volume -= 2
     }
+    //  Método estático
+    static soma(x, y){
+        return x + y;
+        // console.log('Ok, vou trocar.');
+    }
 }
-const controle1 = new ControleRemoto()
+const controle1 = new ControleRemoto('LG')
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+console.log(ControleRemoto.soma(2, 6))     // Método estático -> Só funciona na classe
+console.log(controle1); 
