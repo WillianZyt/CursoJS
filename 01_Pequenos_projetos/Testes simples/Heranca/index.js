fd7a8c697436247e56050b5aabbf7f7819df1731
Willian Zytkoski <88804245+WillianZyt@users.noreply.github.com>
2024-03-24 21:44:23 -0300

    s

--- 01_Pequenos_projetos/Testes simples/Heranca/index.js
+++ 01_Pequenos_projetos/Testes simples/Heranca/index.js
@@ -1,13 +1,17 @@
-class Carro {
-  constructor(marca, modelo) {
+class Veiculo {
+  constructor(tipo, marca, modelo) {
+    this.tipo = tipo;
     this.marca = marca;
     this.modelo = modelo;
+    this.ano = ano;
   }
   carroCompleto = function(marca, modelo){
-    return `Seu veiculo é um ${marca} ${modelo}?`
+    return `Seu veiculo é um ${this.marca} ${this.modelo}?`
   }
 }
 
-const c1 = new Carro('Volkswagen', 'Gol')
-console.log(c1.carroCompleto())
-c1.carroCompleto
+const v1 = new Veiculo('Carro', 'Volkswagen', 'Gol')
+Veiculo.prototype.ano
+const v2 = new Veiculo('Onibus', 'Mercedes', 'B400', 2020)
+console.log(v1.ano)
+console.log(`Seu veiculo é um ${v2.tipo} da marca ${v2.marca} e do ano ${v2.ano}`)
